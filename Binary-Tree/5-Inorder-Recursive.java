31604bf8555f9a452c52fb0a09daa19f75015396
tanmay <tanmaykanwar91@gmail.com>
2026-01-01 21:00:22 +0530

    Inorder traversal recursive approach

---
+++ Binary-Tree/5-Inorder-Recursive.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class MainClass{
+    public static void inorderRecursive(Node root, ArrayList<Integer> res){
+        if(root == null)
+            return;
+        inorderRecursive(root.left,res);
+        res.add(root.data);
+        inorderRecursive(root.right,res);
+    }
+
+    public static void main(String arg[]){
+        Node firstNode = new Node(1);
+        Node secondNode = new Node(2);
+        Node thirdNode = new Node(3);
+        Node fourthNode = new Node(4);
+        
+        firstNode.left = secondNode;
+        firstNode.right = thirdNode;
+        secondNode.left = fourthNode;
+
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        inorderRecursive(firstNode,res);
+        System.out.println(res);
+    }
+}
