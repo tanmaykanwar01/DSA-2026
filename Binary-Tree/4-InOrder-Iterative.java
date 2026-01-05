e39422ef602af0e46c05d010c596c4bab7d667bb
tanmay <tanmaykanwar01@gmail.com>
2026-01-05 21:02:00 +0530

    Inorder traversal iterative approach

--- Binary-Tree/4-InOrder-Iterative.java
+++ Binary-Tree/4-InOrder-Iterative.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Node{
     int data;
     Node left;
@@ -10,6 +12,22 @@ class Node{
 }
 
 class MainClass{
+    public static ArrayList<Integer> inorderIterative(Node root){
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Stack<Node> stack = new Stack<Node>();
+        Node curr = root;
+        while(curr!=null || !stack.isEmpty()){
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.data);
+            curr = curr.right;
+        }
+        return res;
+    }
+
     public static void main(String arg[]){
         Node firstNode = new Node(1);
         Node secondNode = new Node(2);
@@ -19,5 +37,8 @@ class MainClass{
         firstNode.left = secondNode;
         firstNode.right = thirdNode;
         secondNode.left = fourthNode;
+
+        ArrayList<Integer> res = inorderIterative(firstNode);
+        System.out.println(res);
     }
 }
